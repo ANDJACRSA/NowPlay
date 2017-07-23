45da2e3b90fb63733fa85910030af81717f939cd
davidep4 <davidepacioni99@gmail.com>
2017-07-23 15:05:14 +0200

    Aggiunto layout activity login

--- app/src/main/java/com/example/jacopo/anew/SignupActivity.java
+++ app/src/main/java/com/example/jacopo/anew/SignupActivity.java
@@ -2,7 +2,6 @@ package com.example.jacopo.anew;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,8 +9,7 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
     }
 
